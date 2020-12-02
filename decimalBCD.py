287aa08fcee6be78ff09a2584982b7e0a22d71db
Pranav Mital <pranavmital27@gmail.com>
2020-12-02 23:09:17 +0530

    Decimal to BCD Converter
    Needed this for a college quiz so I thought I'd write a program for it and through it, revise the syntax for dictionaries in Python.

---
+++ decimalBCD.py
@@ -0,0 +1,15 @@
+#convert decimal numbers to BCD
+#Pranav Mital
+
+mapping = {0: '0000', 1: '0001', 2: '0010', 3: '0011', 4: '0100', 5: '0101', 6: '0110', 7: '0111', 8: '1000', 9: '1001'}
+
+usr_input = str(input("Please enter a decimal number: "))
+output = ''
+
+for i in usr_input:
+    output += mapping[int(i)] + ' '
+
+print(output)
+
+
+
